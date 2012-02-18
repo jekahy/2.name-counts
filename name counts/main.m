2c5241515134cf1d0f1ed734731bc66e5e1c7fa6
Mac <mahmood1@Mahmood1s-Mac.local>
2012-02-18 11:50:25 +0100

    Initial Commit

---
+++ name counts/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  name counts
+//
+//  Created by Жека Г on 2/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
