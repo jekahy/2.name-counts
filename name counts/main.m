26c1764fcd318165ce91612d41dd85602d31d90e
Eugene Hyrol <jekahy@gmail.com>
2012-02-22 21:48:20 +0100

    update

--- name counts/main.m
+++ name counts/main.m
@@ -12,9 +12,40 @@ int main (int argc, const char * argv[])
 {
 
     @autoreleasepool {
+       
+        NSMutableDictionary *dict=[[NSMutableDictionary alloc]init];
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+       
+        int x=1;
+        while(true){
+            char buf[100];
+            
+            printf("Enter name:");
+            
+            
+            gets(buf);
+            NSString *string=[NSString stringWithCString:buf encoding:NSASCIIStringEncoding];
+            [string stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+            if(string.length==0 || string==(id)[NSNull null] || [string isEqualToString:@"(null)"] || string==nil) {
+                break; 
+            } else{               
+                
+                if([dict objectForKey:string]!=nil){
+                    int d=[[dict objectForKey:string] intValue]+1;
+                    [dict setValue:[NSNumber numberWithInt:d] forKey:string];
+                } else{
+                    [dict setValue:[NSNumber numberWithInt:x] forKey:string];
+
+                }                                 
+                
+            
+            }            
+            
+            
+        }
+        for(id key in dict){
+            NSLog(@"key:%@ value:%@",key,[dict objectForKey:key]);
+        }       
         
     }
     return 0;
